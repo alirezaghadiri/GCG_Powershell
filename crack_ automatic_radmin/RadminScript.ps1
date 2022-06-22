a94aaa11c8e5e701cee6b972cac79f9111b0a0d5
alreza <ghalireza72@gmail.com>
2022-06-22 20:06:34 +0430

    create script

---
+++ crack_ automatic_radmin/RadminScript.ps1
@@ -0,0 +1,13 @@
+﻿$username = '.\gadmin'
+$password = 'Roy@lG@rden57'
+$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential $username, $securePassword
+try{
+Start-Process powershell.exe -Credential $Credential -ArgumentList ("Add-MpPreference -ExclusionPath \\orit01\ITShare\Remote") -NoNewWindow
+echo " add ExclusionPath "
+}
+catch {
+echo "Problem Add Exclusion Path "
+}
+
+Start-Process -FilePath \\orit01\ITShare\Remote\Crack\install.bat -Credential $credential
